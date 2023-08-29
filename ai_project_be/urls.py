740e27e223428f6a4d371de768a0446d4c9e3c1d
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-29 10:23:16 -0600

    add post create projects path

--- ai_project_be/urls.py
+++ ai_project_be/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from project import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/users/<int:id>/projects/', views.generate_project)
 ]
