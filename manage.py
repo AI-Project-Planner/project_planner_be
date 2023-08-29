eac15e50442c68383389b0d8be682a411da0e2a6
Garrett Gregor <118634754+garrettgregor@users.noreply.github.com>
2023-08-29 08:40:51 -0600

    Update manage.py
    remove commented code

--- manage.py
+++ manage.py
@@ -2,8 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import dotenv
-
 
 def main():
     """Run administrative tasks."""
@@ -21,15 +19,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-    # dotenv.read_dotenv(override=True)
-
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-    # try:
-    #     from django.core.management import execute_from_command_line
-    # except ImportError as exc:
-    #     raise ImportError(
-    #         "Couldn't import Django. Are you sure it's installed and "
-    #         "available on your PYTHONPATH environment variable? Did you "
-    #         "forget to activate a virtual environment?"
-    #     ) from exc
-    # execute_from_command_line(sys.argv)
+   
