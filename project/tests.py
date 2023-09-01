93829184633cd8f42cffd9719447d5ab3dd853af
Amy Spears <amspears007@gmail.com>
2023-09-01 14:51:40 -0600

    Update project serializer and test with technologies
    Co-authored-by: Garrett Gregor <118634754+garrettgregor@users.noreply.github.com>
    Co-authored-by: Amy Spears <113188388+amspears007@users.noreply.github.com>
    Co-authored-by: Javen Wilson <123218127+javenb022@users.noreply.github.com>
    Co-authored-by: Michael Callahan <118235615+calforcal@users.noreply.github.com>

--- project/tests.py
+++ project/tests.py
@@ -5,8 +5,8 @@ from user.models import User
 class ProjectModelTest(TestCase):
     def setUp(self):
         self.u = User.objects.create(name='Taylor Swift', email='erastour@gmail.com')
-        self.p = Project.objects.create(user_id=self.u, name='Eras Tour', description='Greatest concert on the planet', steps='Lights, camera, action!', colors='Black', features='Fire, Fireworks, slay', interactions='Get tickets, go to concert, scream', timeline='days', timeline_int=1, tagline='Best Concert Eva!!!', collaborators=2)
-        self.q = Project.objects.create(user_id=self.u, saved=True, name='Eras Tour', description='Greatest concert on the planet', steps='Lights, camera, action!', colors='Black', features='Fire, Fireworks, slay', interactions='Get tickets, go to concert, scream', timeline='days', timeline_int=5, tagline='Best Concert Eva!!!', collaborators=2)
+        self.p = Project.objects.create(user_id=self.u, name='Eras Tour', description='Greatest concert on the planet', steps='Lights, camera, action!', colors='Black', features='Fire, Fireworks, slay', technologies='react, typescript and javascript', interactions='Get tickets, go to concert, scream', timeline='days', timeline_int=1, tagline='Best Concert Eva!!!', collaborators=2)
+        self.q = Project.objects.create(user_id=self.u, saved=True, name='Eras Tour', description='Greatest concert on the planet', steps='Lights, camera, action!', colors='Black', features='Fire, Fireworks, slay', technologies='react, typescript and javascript', interactions='Get tickets, go to concert, scream', timeline='days', timeline_int=5, tagline='Best Concert Eva!!!', collaborators=2)
         global c
         c = Client()
 
@@ -22,12 +22,12 @@ class ProjectModelTest(TestCase):
         self.assertEqual(self.p.steps, 'Lights, camera, action!')
         self.assertEqual(self.p.colors, 'Black')
         self.assertEqual(self.p.features, 'Fire, Fireworks, slay')
+        self.assertEqual(self.p.technologies, 'react, typescript and javascript')
         self.assertEqual(self.p.interactions, 'Get tickets, go to concert, scream')
-
         self.assertEqual(self.p.timeline, 'days')
+        self.assertEqual(self.p.timeline_int, 1)
         self.assertEqual(self.p.tagline, 'Best Concert Eva!!!')
         self.assertEqual(self.p.collaborators, 2)
-
         self.assertEqual(self.p.saved, False)
 
     def test_project_is_returned(self):
@@ -46,6 +46,7 @@ class ProjectModelTest(TestCase):
         self.assertContains(response, 'attributes')
         self.assertContains(response, 'user_id')
         self.assertContains(response, 'name')
+        self.assertContains(response, 'technologies')
         self.assertContains(response, 'description')
         self.assertContains(response, 'features')
         self.assertContains(response, 'interactions')
