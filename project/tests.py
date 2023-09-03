4481504b133af648275b7b98682b3c82dd832d52
Garrett Gregor <garrett.gregor@gmail.com>
2023-09-03 10:57:39 -0600

    WIP add put endpoint

--- project/tests.py
+++ project/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase, Client
 from .models import Project
 from user.models import User
+from importlib import reload
+import code
 
 class ProjectModelTest(TestCase):
     def setUp(self):
@@ -101,6 +103,40 @@ class ProjectModelTest(TestCase):
 
         response = c.post("/api/v1/users/-1/projects/", data=payload, content_type='application/json')
         self.assertEqual(response.status_code, 404)
+
+    def test_project_all_fields_updated(self):
+        payload = {
+            "name": "UPDATED: TaskMaster Pro",
+            "steps": "UPDATED: Project Setup: Create Git repository and define project structure\nBackend Setup: Develop Express.js application, set up API routes\nDatabase Design: Design and implement database schema",
+            "description": "UPDATED: TaskMaster Pro is an all-inclusive task management application designed to optimize team collaboration and productivity.",
+            "features": "UPDATED: User registration and login\nCreate, assign, update, and track tasks\nReal-time collaboration and updates\nPriority-based task categorization",
+            "interactions": "UPDATED: User logs in to TaskMaster Pro account.\nDashboard displays tasks by priority: High, Medium, Low.\nUser adds a task, assigns it, and sets a due date.\nTask appears under the respective priority category.\nAssigned user starts task, status updates in real-time.\nUpon completion, task is marked as done and updates for all.",
+            "colors": "UPDATED: #3498DB\n#27AE60\n#F39C12\n#F0F3F4\n#333333\n#E74C3C",
+            "technologies": "UPDATED: react, typescript and javascript",
+            "timeline": "UPDATE",
+            "timeline_int": 10,
+            "tagline": "UPDATED: Stay organized and track progress",
+            "collaborators": 10,
+            "saved": False,
+            "user_id": 1
+        }
+
+        response = c.put(f"/api/v1/users/{self.u.id}/projects/{self.p.id}/", data=payload, content_type='application/json')
+        self.assertEqual(response.status_code, 202)
+        code.interact(local=dict(globals(), **locals()))
+        self.assertEqual(self.p.user_id, self.u)
+        self.assertEqual(self.p.name, payload['name'])
+        self.assertEqual(self.p.description, payload['description'])
+        self.assertEqual(self.p.steps, payload['steps'])
+        self.assertEqual(self.p.colors, payload['colors'])
+        self.assertEqual(self.p.features, payload['features'])
+        self.assertEqual(self.p.technologies, payload['technologies'])
+        self.assertEqual(self.p.interactions, payload['interactions'])
+        self.assertEqual(self.p.timeline, payload['timeline'])
+        self.assertEqual(self.p.timeline_int, payload['timeline_int'])
+        self.assertEqual(self.p.tagline, payload['tagline'])
+        self.assertEqual(self.p.collaborators, payload['collaborators'])
+        self.assertEqual(self.p.saved, payload['saved'])
 
     def test_get_all_users_projects(self):
         response = c.get(f"/api/v1/users/{self.u.id}/projects/")
--- project/views.py
+++ project/views.py
@@ -3,8 +3,9 @@ from user.models import User
 from .serializers import ProjectSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
 from rest_framework import status
-import requests, json, os
+import requests, json, os, code
 
 @api_view(['GET', 'POST'])
 def generate_project(request, id):
@@ -85,7 +86,7 @@ def generate_project(request, id):
         projects = Project.objects.all()
         return Response(Project.serialize_all_projects(projects), status=status.HTTP_200_OK)
 
-@api_view(['PATCH'])
+@api_view(['PUT', 'PATCH'])
 def update_project(request, user_id, project_id):
     try:
         project = Project.objects.get(id=project_id)
@@ -95,13 +96,27 @@ def update_project(request, user_id, project_id):
             "Status": 404
         }
         return Response(response, status=status.HTTP_404_NOT_FOUND)
-    parsed_request = json.loads(request.body)
-    saved_status = parsed_request['saved']
-    if saved_status=='true':
-        saved_status=True
-    elif saved_status=='false':
-        saved_status=False
-    project.saved=saved_status
-    project.save()
-    serializer = ProjectSerializer(project)
-    return Response(Project.serialize_project(serializer, project_id), status=status.HTTP_202_ACCEPTED)
+
+    if request.method == 'PUT':
+        project_serializer = ProjectSerializer(project, data=request.data)
+        if project_serializer.is_valid():
+            project_serializer.save()
+            return Response(Project.serialize_project(project_serializer, project_id), status=status.HTTP_202_ACCEPTED)
+        else:
+            response = {
+                "Error": f"{project_serializer.errors}",
+                "Status": 400
+            }
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'PATCH':
+        parsed_request = json.loads(request.body)
+        saved_status = parsed_request['saved']
+        if saved_status=='true':
+            saved_status=True
+        elif saved_status=='false':
+            saved_status=False
+        project.saved=saved_status
+        project.save()
+        serializer = ProjectSerializer(project)
+        return Response(Project.serialize_project(serializer, project_id), status=status.HTTP_202_ACCEPTED)
