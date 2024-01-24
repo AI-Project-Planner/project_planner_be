204db0b290fb82746394b4dbb0fb452814e1a21d
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-24 09:52:43 -0700

    refactor: remove redundant post view for user

--- user/views.py
+++ user/views.py
@@ -19,15 +19,6 @@ def show_user(request, id):
     return Response(User.serialize_user(serializer, user.id), status=status.HTTP_200_OK)
 
 @api_view(['POST'])
-def generate_user(request):
-    parsed_request = json.loads(request.body)
-    name = parsed_request['name']
-    email = parsed_request['email']
-    created_user = User.objects.create(name=name, email=email)
-    serializer = UserSerializer(created_user)
-    return Response(User.serialize_user(serializer, created_user.id), status=status.HTTP_201_CREATED)
-
-@api_view(['POST'])
 def find_or_create_user(request):
     parsed_request = json.loads(request.body)
     name = parsed_request['name']
